6491948717ce92199ccd924bd6c70951609e803a
sophiebremer <40056287+sophiebremer@users.noreply.github.com>
2018-10-08 10:35:46 +0200

    Add some test declarations.

---
+++ modules/vector.d.ts
@@ -0,0 +1,15 @@
+import * as Highcharts from "../highcharts";
+/**
+ * Adds the module to the imported Highcharts namespace.
+ *
+ * @param highcharts
+ *        The imported Highcharts namespace to extend.
+ */
+declare function factory(highcharts: typeof Highcharts): void;
+/**
+ * The Highcharts object is the placeholder for all other members, and various
+ * utility functions. The most important member of the namespace would be the
+ * chart constructor.
+ */
+declare module "../highcharts" {}
+export = factory;
